c359c420454d994e121f75eaec7bbe2681fccaa5
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-07-20 19:30:21 -0400

    Add list of all products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,20 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products_array = Product.all
+    render :index
+  end
+
   def show
     @the_product = Product.find(params[:id])
     render :show
+  end
+
+  def destroy
+    @the_product = Product.find(params[:id])
+    @the_product.destroy
+
+    redirect_to "/products"
   end
 
 end
