8a608e187928ce299221a6eca3fe72916529d439
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-07-20 17:20:24 -0400

    Add seed file for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: "Han Solo", email: "han@smugglers.sexy")
+User.create!(name: "Chewbacca", email: "chewie@smugglers.sexy")
+User.create!(name: "Rey", email: "rey@smugglers.sexy")
